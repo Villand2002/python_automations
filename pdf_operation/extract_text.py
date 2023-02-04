06d544cb0b6577274897ac84096c69f67a8f37de
ST <st20027802@gmail.com>
2023-02-04 13:31:36 +0900

    pdfのテキスト抽出を実装

--- pdf_operation/extract_text.py
+++ pdf_operation/extract_text.py
@@ -4,17 +4,19 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 
+from pdfminer.high_level import extract_text
+from pdfminer.high_level import extract_text_to_fp
+
+pdf_path=(input("ファイルパスを指定:"))
+
+text = extract_text(pdf_path)
+print(text)
+
+# 参考:https://office54.net/python/module/pdf-pdfminer-six#section1-2
+
+
+
+
+
 # pip install PyPDF2
 from PyPDF2 import PdfFileReader
-
-with open("2019-11-Chaos_Engineering_Whitepaper.pdf", "rb") as input:
-    reader = PdfFileReader(input)
-
-    # pdfの総ページ数は？
-    print("2019-11-Chaos_Engineering_Whitepaper.pdf.pdf has %d pages.\n" % reader.getNumPages())
-
-# 指定のページのデータを読み込む
-    page = reader.getPage(0)
-
-    # 読み込んだページのテキストを抽出
-    print(page.extractText())
