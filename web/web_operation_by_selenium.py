49548e490d95d5a95a135f4c1da5f2c6036179ec
ST <st20027802@gmail.com>
2023-02-05 13:24:48 +0900

    コメントを追加

--- web/web_operation_by_selenium.py
+++ web/web_operation_by_selenium.py
@@ -11,3 +11,7 @@ driver.get("https://www.google.co.jp/")
 
 time.sleep(3)
 driver.quit()
+
+
+# 参考:https://tech-blog.rakus.co.jp/entry/20221222/selenium#%E8%A6%81%E7%B4%A0%E3%81%AE%E5%8F%96%E5%BE%97
+# https://reffect.co.jp/python/selenium#chromedriver
