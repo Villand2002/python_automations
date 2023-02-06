35be1dde1598ef47c48aa742b6cf313c9fe5fd73
ST <st20027802@gmail.com>
2023-02-06 17:14:53 +0900

    参考url追加

--- login_operate.py/auto_login.py
+++ login_operate.py/auto_login.py
@@ -59,3 +59,4 @@ password.submit()
 # 参考:https://self-development.info/selenium-4%E3%81%A7%E3%80%8Cdeprecationwarning%E3%80%8D%E3%81%8C%E5%87%BA%E3%82%8B%E5%A0%B4%E5%90%88%E3%81%AE%E5%AF%BE%E7%AD%96/
 # https://yaspage.com/yasunolog/python-selenium1/
 # 自動ログイン:https://qiita.com/wejhhv/items/59d739b40dc6f7f5aabc
+# version更新:https://bluegoat.jp/blog/chrome-driver/
