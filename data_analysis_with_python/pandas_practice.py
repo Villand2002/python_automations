85e60363ee1c0bdd7bd1ba3bcf8d26d7e1d21665
ST <st20027802@gmail.com>
2023-02-13 12:07:54 +0900

    pandas練習

--- data_analysis_with_python/pandas_practice.py
+++ data_analysis_with_python/pandas_practice.py
@@ -1,1 +1,20 @@
 import pandas as pd
+df=pd.read_csv("C:\\Users\\st200\\OneDrive - Kyushu University\\個人学習\\python\\train.csv")
+# 最初の5行を出力
+df.head()
+# print(df.head) 確認用
+
+# df中の列名を取得
+print(df.columns)
+
+# nan値をdropする
+df=df.dropna()
+
+print(df)
+
+print(df["Embarked"])
+
+
+# dummy変数に変換
+print(pd.get_dummies(df["Embarked"])
+)
